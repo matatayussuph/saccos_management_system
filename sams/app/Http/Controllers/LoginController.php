5f9ad9c0aed5b23bee84987d5eef4655bb645d9a
navycode255 <maternusmj@gmail.com>
2022-09-02 10:17:00 +0300

    Newly created Project

---
+++ sams/app/Http/Controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    
+   protected function authenticated(Request $request, $user){
+
+        $user = generateTwoFactorCode();
+        $user->notify(new TwoFactorCode());
+
+   }
+}
